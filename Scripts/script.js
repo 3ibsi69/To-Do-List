d83e3f33ffb5d3d4570982a7f3ef50cdc0195b7b
3ibsi69 <a.abbassi21306@pi.tn>
2023-06-12 20:43:05 +0100

    fixed

---
+++ Scripts/script.js
@@ -0,0 +1,46 @@
+const input = document.getElementById("input");
+const list = document.getElementById("list-container");
+function addTask() {
+  if (input.value === "") {
+    alert("Please enter a task");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = input.value;
+    list.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "&#10006;";
+    li.appendChild(span);
+    let edit = document.createElement("p");
+    edit.innerHTML = "&#9998;";
+    li.appendChild(edit);
+  }
+
+  input.value = "";
+  savedata();
+}
+//add
+list.addEventListener("click", function (e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+    savedata();
+  }
+  //delete
+  else if (e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+    savedata();
+    //edit
+  } else if (e.target.tagName === "P") {
+    let edit = prompt("Edit your task");
+    e.target.parentElement.firstChild.nodeValue = edit;
+    savedata();
+  }
+});
+
+//local storage
+function savedata() {
+  localStorage.setItem("list", list.innerHTML);
+}
+function getdata() {
+  list.innerHTML = localStorage.getItem("list");
+}
+getdata();
